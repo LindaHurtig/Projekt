8f7d70ce42f268a4cae271b08c746722b489c578
Linda Hurtig <lindahurtig93@gmail.com>
2021-12-07 12:41:50 +0100

    stopklocka

--- src/time/StopWatch.java
+++ src/time/StopWatch.java
@@ -1,21 +1,49 @@
+
 package time;
 
+import java.util.concurrent.TimeUnit;
+
 public class StopWatch {
-	int hours;
-	int minutes;
-	int seconds;
 
 
-int [] receiveCurrentTime(){
-	
-	int[] currentTime = new int[2];
-	
-	currentTime[0] = hours;
-	currentTime[1] = minutes;
-	currentTime[2] = seconds;
-	
-	System.out.println(currentTime);
-	
-	return currentTime;
-}
+	public static boolean stopWatchRunning;
+
+	public static long startTime;
+	public static long stopTime;
+
+	public static long stopWatchStart() {
+
+		startTime = System.currentTimeMillis();
+		stopWatchRunning = true;
+
+		return startTime;
+
+	}
+
+	public static long stopWatchStop()  {
+
+		stopTime = System.currentTimeMillis();
+		stopWatchRunning = false;
+
+		return stopTime;
+
+	}
+
+	public static String timePast() {
+
+		long timeP = stopTime - startTime;
+
+		long hours = TimeUnit.MILLISECONDS.toHours(timeP);
+		timeP -= TimeUnit.HOURS.toMillis(hours);
+		long min = TimeUnit.MILLISECONDS.toMinutes(timeP);
+		timeP -= TimeUnit.MINUTES.toMillis(min);
+		long sek = TimeUnit.MILLISECONDS.toSeconds(timeP);
+		timeP -= TimeUnit.SECONDS.toMillis(sek);
+
+		String timeConverted = (hours + ":" + min + ":" + sek + ":" + timeP);
+
+		return timeConverted;
+
+	}
+
 }
